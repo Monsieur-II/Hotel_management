4fe9a7e098881f0d1f039feeb919413863e89ada
Lamber22 <Lamberjoe5@gmail.com>
2024-04-02 00:47:01 +0000

    update

--- models/room.py
+++ models/room.py
@@ -6,7 +6,7 @@ class Room(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     room_number = db.Column(db.String(50), unique=True, nullable=False)
-    room_type = db.Column(db.String(50), nullable=False)  # e.g. single, double, suite
+    room_type = db.Column(db.String(50), nullable=False)
     price_per_night = db.Column(db.Float, nullable=False)
     is_available = db.Column(db.Boolean, default=True)
     available_date = db.Column(db.Date, nullable=False)
